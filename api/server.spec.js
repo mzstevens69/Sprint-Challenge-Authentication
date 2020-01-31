6da4e1d4b579a89de3277b319ee31fd767563ccc
Michael Stevens <stevensmike36@yahoo.com>
2020-01-31 13:03:22 -0600

    mvp

---
+++ api/server.spec.js
@@ -0,0 +1,58 @@
+const server = require('./server')
+const db = require('../database/dbConfig')
+const request = require('supertest')
+// is the test working
+
+describe('server', () => {
+    it('runs the tests', () => {
+        expect(true).toBe(true)
+    })
+})
+// is test showing process.env.DB_ENV=testing
+describe('GET /', () => {
+    it('has environment process as testing', () => {
+        expect(process.env.DB_ENV).toBe('testing')
+    })
+})
+
+describe('POST /api/auth/register', () => {
+   beforeEach(async () => {
+       await db('users').truncate()
+   })
+    it('register users should return 201', () => {
+        return request(server).post('/api/auth/register')
+        .send({ username: 'test0', password: 'test0'})
+        .then(res => {
+            expect(res.status).toBe(201)
+        })
+        
+    })
+    it('returns username', () => {
+        let user = { username: 'test00', password: 'test00' }
+        return request(server).post('/api/auth/register')
+        .send(user)
+        .then(res => {
+            expect(res.body.username).toMatch('test00')
+        })
+
+    })
+
+})
+let token
+describe('POST /api/auth/login', () => {
+    beforeEach(async () => {
+        await db('users').truncate()
+    })
+    
+    it('return 200', () => {
+        let user = { username: 'test000', password: 'test000'}
+        return request(server).post('/api/auth/login')
+        .send(user)
+        .then(res => {
+            200, { message: `Welcome ${user.username} you are logged in !`, token: res.body.token };
+      })
+   
+    })
+
+})
+
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,34 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken')
+const { jwtSecret } = require('../config/secrets')
+//const Users = require('../users/users-model.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+  const token = req.headers.authorization
+
+    if (token) {
+      
+      jwt.verify(token, jwtSecret, (err, decodedToken) => {
+
+        if(err) {
+// the token is not valid
+          res.status(401).json({
+            error: "your wheels are spinning."
+          })
+
+        } else {
+
+          req.user =  decodedToken.user;
+
+          next()
+        }
+      })
+
+    } else {
+      res.status(401).json({
+        BlackKnight: "None shall pass!, "
+      })
+    }
+  }
